90269675b17e7c32b4076c285ef7484cf16e8dbb
Daryll Santos <daryll.santos@gmail.com>
2025-04-19 13:20:44 +0800

    Try bare storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,38 +5,11 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@chromatic-com/storybook',
-    '@storybook/experimental-addon-test',
-  ],
+  stories: ['../src/**/*.storiesssssssss.@(js|jsx|mjs|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/experimental-nextjs-vite',
     options: {},
-  },
-  staticDirs: ['../public'],
-  viteFinal: async (config) => {
-    if (config.resolve) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': path.resolve(__dirname, '../src'),
-      };
-    }
-
-    console.log('config', config);
-
-    if (config.build) {
-      config.build.commonjsOptions = {
-        ...config.build.commonjsOptions,
-        transformMixedEsModules: true,
-      };
-    }
-
-    console.log('config.build', config.build);
-
-    return config;
   },
 };
 

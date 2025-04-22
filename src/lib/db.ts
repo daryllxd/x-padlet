90e9471bdb5646b8e5352aeaea84531786387aa0
Daryll Santos <daryll.santos@gmail.com>
2025-04-22 12:08:27 +0800

    Use IIFE to fix Typescript issue with potentially missing Supabase

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,18 +3,19 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-let supabase: ReturnType<typeof createClient>;
-
-if (supabaseUrl && supabaseKey) {
-  supabase = createClient(supabaseUrl, supabaseKey);
-} else {
-  console.error(
-    '\x1b[31m%s\x1b[0m',
-    '❗️ Missing Supabase environment variables. Supabase features will not be available.'
-  );
-  if (!supabaseUrl) console.error('\x1b[31m%s\x1b[0m', '❗ Missing NEXT_PUBLIC_SUPABASE_URL');
-  if (!supabaseKey) console.error('\x1b[31m%s\x1b[0m', '❗ Missing NEXT_PUBLIC_SUPABASE_ANON_KEY');
-}
+const supabase = (() => {
+  if (!supabaseUrl || !supabaseKey) {
+    console.error(
+      '\x1b[31m%s\x1b[0m',
+      '❗️ Missing Supabase environment variables. Supabase features will not be available.'
+    );
+    if (!supabaseUrl) console.error('\x1b[31m%s\x1b[0m', '❗ Missing NEXT_PUBLIC_SUPABASE_URL');
+    if (!supabaseKey)
+      console.error('\x1b[31m%s\x1b[0m', '❗ Missing NEXT_PUBLIC_SUPABASE_ANON_KEY');
+    return {} as ReturnType<typeof createClient>;
+  }
+  return createClient(supabaseUrl, supabaseKey);
+})();
 
 export { supabase };
 
